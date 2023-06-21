cc81b5579e0cb41e40b38b75734a75884131f8b0
Adib-96 <adibbh1996@gmail.com>
2023-06-21 15:35:40 -0700

    insominia

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -15,14 +15,3 @@ i++;
 }
 _putchar('\n');
 }
-/**
-*main - Prints ==> program that prints the alphabet, in lowercase,
-*followed by a new line.
-*The output of the program:
-*Return: Always 0.
-*/
-
-int main(void){
-print_alphabet();
-    return (0);
-}
