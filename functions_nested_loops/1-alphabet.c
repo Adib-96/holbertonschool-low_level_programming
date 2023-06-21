d3532500ac997bbe976afe269e073473c15339b6
Adib-96 <adibbh1996@gmail.com>
2023-06-21 13:06:21 -0700

    I play what I call the alphabet game

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+*print_alphabet - Prints ==> program that use a while loop
+*to generate characters
+*from a..z in lower case.
+*/
+void print_alphabet(void)
+{
+int i = 97;
+while (i <= 122)
+{
+_putchar(i);
+i++;
+}
+_putchar('\n');
+}
+/**
+*main - Prints ==> program that prints the alphabet, in lowercase,
+*followed by a new line.
+*The output of the program:
+*Return: Always 0.
+*/
+int main(void)
+{
+print_alphabet();
+return (0);
+}
+
