e624254a461d7cb71fcc95a60225c7068f0e19fd
Adib-96 <adibbh1996@gmail.com>
2023-06-30 04:32:25 -0700

    Printing half of a string

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -12,9 +12,19 @@ while (str[len] != '\0')
 {
 len++;
 }
-for (i = (len - 1) / 2; i <= len - 1; i++)
+
+if(len % 2 == 0){
+for (i = len / 2; i <= len - 1; i++)
 {
 _putchar(str[i]);
+}
+}
+else 
+{
+for(j = (len - 1) / 2; j <= len - 1; i++)
+{
+_putchar(str[j]);
+}
 }
 _putchar('\n');
 }
