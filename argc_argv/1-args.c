6ffb9ea41c08ab564505f82e35b7cbe4cd454a3b
Adib-96 <adibbh1996@gmail.com>
2023-07-07 12:40:13 -0700

    number of args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print the number of command-line arguments
+ *@argc:ARGument Count
+ *@*argv:ARGument Vector
+ *Return:0
+*/
+
+int main(int argc,char *argv[])
+{
+printf("%d",argc);
+return (0);
+}
