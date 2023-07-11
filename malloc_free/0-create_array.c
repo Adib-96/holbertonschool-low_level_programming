dffffa6b469b9f911ce3c417bc8b3e93d683b53c
Adib-96 <adibbh1996@gmail.com>
2023-07-11 11:13:41 -0700

    dynamic memory

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include  <stdio.h>
+
+char *create_array(unsigned int size,char c) {
+    if (size == 0)
+    {
+        return NULL;
+    }
+    else 
+    {
+        char *p = (char*)malloc(size *sizeof(c));
+    }
+
+    
+}
---
+++ malloc_free/main.h
@@ -0,0 +1,6 @@
+#ifndef MEMORY_FILE
+#define MEMORY_FILE
+
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+#endif
