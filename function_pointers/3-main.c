93e8bad445a696c7cc82b18279a3238eb2e4b835
Adib-96 <adibbh1996@gmail.com>
2023-07-22 09:22:38 -0700

    Convert DOS/Windows line endings to Linux line endings

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,22 +1,22 @@
 #include "3-calc.h"
 #include <stdio.h>
+
 /**
- * main - program that perfroms simple operations
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: Always 0 (Success)
- */
-int main (int argc, char *argv[]) 
+* main - program that perfroms simple operations
+* @argc: number of arguments
+* @argv: array of arguments
+*
+* Return: Always 0 (Success)
+*/
+int main(int argc, char *argv[])
 {
 int arg1, arg2, result;
 char o;
-int (*func)(int,int);
-
+int (*func)(int, int);
 if (argc != 4)
 {
-    printf("Error\n");
-    exit(98);
+printf("Error\n");
+exit(98);
 }
 arg1 = atoi(argv[1]);
 arg2 = atoi(argv[3]);
@@ -34,5 +34,5 @@ exit(100);
 }
 result = func(arg1, arg2);
 printf("%d\n", result);
-return (0);    
+return (0);
 }
