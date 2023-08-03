71a4a4c79f2df09c991f2ca1410bb8444170e34f
Adib-96 <adibbh1996@gmail.com>
2023-08-02 17:54:11 -0700

    execute before the entry point(main function)

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+
+/**
+ *before_main - Print text before main func is executed
+ */
+void before_main(void)
+{
+printf("You're beat! and yet, you must allow,");
+printf("\nI bore my house upon my back!\n");
+}
--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -23,4 +23,6 @@ struct list_s *next;
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+void before_main() __attribute__((constructor));
+
 #endif
