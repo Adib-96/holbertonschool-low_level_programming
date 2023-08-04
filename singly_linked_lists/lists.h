7be375f737b6e23fa817a8248033ffccb30a7786
Adib-96 <adibbh1996@gmail.com>
2023-08-04 08:55:22 -0700

    Add node

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,9 +1,11 @@
 #ifndef SINGLE_L
 #define SINGLE_L
 
-
+#include <stdlib.h>
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -24,5 +26,5 @@ int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 void before_main() __attribute__((constructor));
-
+list_t *add_node(list_t **head, const char *str);
 #endif
