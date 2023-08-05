780336cdf68e2567c928fcabb289aea4571c82ab
Adib-96 <adibbh1996@gmail.com>
2023-08-05 15:23:54 -0700

    Print list

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ *print_dlistint - Print list
+ *@h: pointer to node
+ *Return:the number of nodes
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+size_t len = 0;
+while (h)
+{
+printf("%d\n", h->n);
+len++;
+h = h->next;
+}
+return (len);
+}
