4bd6a5adc8777e8081e53995d8dc6275d803ac55
Adib-96 <adibbh1996@gmail.com>
2023-08-22 13:10:01 -0700

    update header file

--- file_io/main.h
+++ file_io/main.h
@@ -1,10 +1,16 @@
 #ifndef HEADER_F
 #define HEADER_F
 
-
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <stddef.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
-
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
 
 #endif
