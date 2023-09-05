42f3f5cee03db20c4d03b78a2455a4556a74bcce
Adib-96 <adibbh1996@gmail.com>
2023-09-05 14:27:08 -0700

    sum data in nodes

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -6,14 +6,15 @@
  *
  * Return: The sum of all the data.
  */
-
 int sum_dlistint(dlistint_t *head)
 {
 	int sum = 0;
+
 	while (head)
 	{
 		sum += head->n;
 		head = head->next;
 	}
+
 	return (sum);
 }
